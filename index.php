0c00b344cbf194094586a9ecbc4bf2ac366776c4
hl <havenlavender228@gmail.com>
2026-03-05 11:52:24 -0500

    stuff

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clock-It</title>
+</head>
+<body>
+    <a href="index.php">dashboard</a>
+    <a href="alarm.php">alarm</a>
+    <a href="calendar.php">alarm</a>
+    <a href="stopwatch.php">alarm</a>
+</body>
+</html>
